776273e89a81fa56ac13e0bb0b879e6b1b3584a1
Angelo Teixeira <imnotteixeira@gmail.com>
2020-03-05 23:30:36 +0000

    Step 1

--- src/components/MyFormDesktop.js
+++ src/components/MyFormDesktop.js
@@ -1,9 +1,86 @@
 import React from "react";
+import { Card,
+    CardHeader,
+    CardContent,
+    TextField,
+    Slider,
+    FormControl,
+    makeStyles,
+    Switch,
+    FormControlLabel,
+    FormGroup,
+    Typography,
+    Button,
+} from "@material-ui/core";
 
-const MyFormDesktop = () => (
-    <div>
-        Desktop Form
-    </div>
-);
+const useStyles = makeStyles((theme) => ({
+    wrapper: {
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    card: {
+        width: "40%",
+        padding: theme.spacing(2),
+    },
+    cardContent: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    form: {
+        width: "70%",
+    },
+    button: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+const MyFormDesktop = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.wrapper}>
+            <Card className={classes.card}>
+                <form>
+                    <CardHeader title="My Form"/>
+                    <CardContent className={classes.cardContent}>
+                        <FormGroup className={classes.form}>
+                            <TextField
+                                name="search"
+                                label="Search"
+                            />
+                            <FormControl margin="normal">
+                                <Typography id="slider-label">
+                            Results per Page
+                                </Typography>
+                                <Slider
+                                    step={10}
+                                    min={10}
+                                    max={50}
+                                    aria-labelledby="slider-label"
+                                />
+                            </FormControl>
+                            <FormControlLabel
+                                control={<Switch checked={true} onChange={() => {}} value="showHidden" />}
+                                label="Show Hidden"
+                                margin="normal"
+                            />
+                            <Button
+                                className={classes.button}
+                                color="primary"
+                                variant="contained"
+                                type="submit"
+                            >
+                            Submit
+                            </Button>
+                        </FormGroup>
+                    </CardContent>
+                </form>
+            </Card>
+        </div>
+    );
+};
+
 
 export default MyFormDesktop;
--- src/components/MyFormMobile.js
+++ src/components/MyFormMobile.js
@@ -1,9 +1,69 @@
 import React from "react";
+import { Dialog,
+    DialogTitle,
+    TextField,
+    Slider,
+    FormControl,
+    Switch,
+    FormControlLabel,
+    FormGroup,
+    Typography,
+    makeStyles,
+    DialogContent,
+    Button,
+    DialogActions,
+} from "@material-ui/core";
 
-const MyFormMobile = () => (
-    <div>
-        Mobile Form
-    </div>
-);
+const useStyles = makeStyles((theme) => ({
+    actions: {
+        margin: theme.spacing(2),
+    },
+}));
+
+const MyFormMobile = () => {
+    const classes = useStyles();
+    return (
+        <Dialog open fullScreen>
+            <form >
+                <DialogTitle>My Form</DialogTitle>
+                <DialogContent>
+                    <FormGroup>
+                        <TextField
+                            name="search"
+                            label="Search"
+                            margin="normal"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={true} onChange={() => {}} value="showHidden" />}
+                            label="Show Hidden"
+                            margin="normal"
+                        />
+                        <FormControl margin="normal">
+                            <Typography id="slider-label">
+                            Results per Page
+                            </Typography>
+                            <Slider
+                                step={10}
+                                min={10}
+                                max={50}
+                                aria-labelledby="slider-label"
+                            />
+                        </FormControl>
+                    </FormGroup>
+                </DialogContent>
+                <DialogActions className={classes.actions}>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        type="submit"
+                    >
+                    Submit
+                    </Button>
+
+                </DialogActions>
+            </form>
+        </Dialog>
+    );
+};
 
 export default MyFormMobile;
